f6f8b908cd10284030e7cb3b4f373f154d0ab145
zhenshan <zhenshan@belmont.tech>
2022-02-19 00:25:53 +0800

    update model path

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 from predict import unzip_model, load_model_and_tokenizer, generate_summary
 
 # get model_zip_path
-model_zip_path = "/Users/zhenshanjin/Documents/Belmont/sandy/UtilityDaisies/DistillBartCNN_6_6/tmp/distilbart-cnn-6-6.zip"
+model_zip_path = "/tmp/distilbart-cnn-6-6.zip"
 model_path = unzip_model(model_zip_path)
 model, tokenizer = load_model_and_tokenizer(model_path)
 
--- predict.py
+++ predict.py
@@ -2,17 +2,18 @@ import os
 import pathlib
 import zipfile
 from transformers import BartTokenizer, BartForConditionalGeneration
+import tempfile
 
+root_folder = tempfile.gettempdir()
 
 def unzip_model(zip_model_path):
     p = pathlib.Path(zip_model_path)
-    root = str(p.parents[0])
-    print(f'start unzip: {root}')
-    model_path = os.path.join(root, os.path.splitext(os.path.basename(zip_model_path))[0] + '/')
+    print(f'start unzip: {root_folder}')
+    model_path = os.path.join(root_folder, os.path.splitext(os.path.basename(zip_model_path))[0] + '/')
     if not os.path.isdir(model_path):
         with zipfile.ZipFile(zip_model_path, "r") as zip_ref:
-            zip_ref.extractall(root)
-        
+            zip_ref.extractall(root_folder)
+
     print(f"finish zipping model to {model_path}")
     return model_path
 
