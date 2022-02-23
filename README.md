3ca1faed69495ce3d04b9026e2aa51f67d915abd
zhenshan <zhenshan@belmont.tech>
2022-02-23 09:24:17 -0600

    add README.md

---
+++ README.md
@@ -0,0 +1,11 @@
+## Overview
+Text summarization. Bart based text summarization
+
+https://huggingface.co/sshleifer/distilbart-cnn-6-6
+
+download model
+```
+git lfs clone https://huggingface.co/sshleifer/distilbart-cnn-6-6
+```
+
+main code: https://huggingface.co/docs/transformers/model_doc/bart#transformers.BartForConditionalGeneration
--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-# from https://huggingface.co/sshleifer/distilbart-cnn-6-6
-
-import torch
-
 from predict import unzip_model, load_model_and_tokenizer, generate_summary
 
 # get model_zip_path
-# model_zip_path = "/pebble_source/c3334ce8-fe28-4f0f-9fc7-a7b203eec73f/distilbart-cnn-6-6.zip"
 # model_path = unzip_model(model_zip_path)
 model_path = "/pebble_tmp/tmp/model_distilbart"
 model, tokenizer = load_model_and_tokenizer(model_path)
